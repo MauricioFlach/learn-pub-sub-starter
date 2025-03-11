2d61939eea433c52c1176261fd3c9dbee212e16b
MauricioFlach <mauricio.p.f@hotmail.com>
2025-03-10 21:21:27 -0300

    visto nack

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/MauricioFlach/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -23,16 +25,55 @@ func main() {
 		log.Fatalf("could not create channel: %v", err)
 	}
 
-	err = pubsub.PublishJSON(
-		publishCh,
-		routing.ExchangePerilDirect,
-		routing.PauseKey,
-		routing.PlayingState{
-			IsPaused: true,
-		},
+	_, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug,
+		routing.GameLogSlug+".*",
+		pubsub.SimpleQueueDurable,
 	)
 	if err != nil {
-		log.Printf("could not publish time: %v", err)
+		log.Fatalf("could not declarate the queue: %v", err)
 	}
-	fmt.Println("Pause message sent!")
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
+
+	gamelogic.PrintServerHelp()
+
+	i := 0
+	for i < 1 {
+		Input := gamelogic.GetInput()
+		if len(Input) > 0 {
+			if Input[0] == "pause" {
+				err = pubsub.PublishJSON(
+					publishCh,
+					routing.ExchangePerilDirect,
+					routing.PauseKey,
+					routing.PlayingState{
+						IsPaused: true,
+					},
+				)
+				if err != nil {
+					log.Printf("could not publish time: %v", err)
+				}
+				fmt.Println("Pause message sent!")
+			} else if Input[0] == "resume" {
+				err = pubsub.PublishJSON(
+					publishCh,
+					routing.ExchangePerilDirect,
+					routing.PauseKey,
+					routing.PlayingState{
+						IsPaused: false,
+					},
+				)
+				if err != nil {
+					log.Printf("could not publish time: %v", err)
+				}
+			}	else if Input[0] == "quit" {
+				i = 1
+				fmt.Println("Exiting the server...")
+			} else {
+				fmt.Println("Unknown command.")
+			}
+		}
+	}
 }
